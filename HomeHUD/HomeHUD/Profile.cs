4ef5b33796b671be28b7eb95329777caaf8af4da
Cody Cao <cody.cao26@gmail.com>
2019-06-24 22:01:21 -0700

    Added Profile class and refactored

---
+++ HomeHUD/HomeHUD/Profile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Calendar.v3.Data;
+
+namespace HomeHUD
+{
+    class Profile
+    {
+        private string name;
+        private string calendarID;
+        private Events events;
+
+        public Profile(string name, string calendarID)
+        {
+            this.name = name;
+            this.calendarID = calendarID;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string CalendarID
+        {
+            get { return calendarID; }
+            set { calendarID = value; }
+        }
+        public Events Events
+        {
+            get { return events; }
+            set { events = value; }
+        }
+
+    }
+}
